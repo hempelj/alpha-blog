8aa674502344ae071090209ac234f4e1fcc45b5c
Rails Udemy Course <jessica.l.hempel@gmail.com>
2015-12-18 19:02:40 +0000

    Added articles listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,12 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def index
+    @articles = Article.all
+  end
 def create  
   @article = Article.new(article_params)  
   if @article.save  
@@ -17,4 +23,13 @@ end
 def article_params  
     params.require(:article).permit(:title, :description)  
   end
+def update  
+  @article = Article.find(params[:id])  
+  if @article.update(article_params) 
+    flash[:notice] = "Article was successfully updated"  
+    redirect_to article_path(@article)  
+  else  
+    render 'edit'  
+  end
+end
 end
