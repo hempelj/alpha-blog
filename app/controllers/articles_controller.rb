96043af5df5d2cfe6f9b6ef257660ce5d7364ca8
Rails Udemy Course <jessica.l.hempel@gmail.com>
2015-12-18 21:24:33 +0000

    Clean up show, edit, destroy, create code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,22 @@
 class ArticlesController < ApplicationController
-  def new
-    @article = Article.new
-  end
-  def edit
-    @article = Article.find(params[:id])
-  end
-  def index
-    @articles = Article.all
-  end
+  
+before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
+def set_article  
+  @article = Article.find(params[:id])  
+end
+    
+def new
+  @article = Article.new
+end
+  
+def edit
+end
+  
+def index
+  @articles = Article.all
+end
+  
 def create  
   @article = Article.new(article_params)  
   if @article.save  
@@ -17,20 +26,21 @@ def create
       render 'new'  
   end 
 end
+
 def show
-  @article = Article.find(params[:id])
 end
+
 def article_params  
-    params.require(:article).permit(:title, :description)  
-  end
+   params.require(:article).permit(:title, :description)  
+end
+
 def destroy  
-  @article = Article.find(params[:id])  
   flash[:notice] = "Article was successfully deleted" 
   @article.destroy  
   redirect_to articles_path
 end
+
 def update  
-  @article = Article.find(params[:id])  
   if @article.update(article_params) 
     flash[:notice] = "Article was successfully updated"  
     redirect_to article_path(@article)  
