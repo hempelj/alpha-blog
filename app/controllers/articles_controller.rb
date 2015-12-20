ac4e22f64a80074d437f8dbd139062a86a8544e2
Rails Udemy Course <jessica.l.hempel@gmail.com>
2015-12-20 18:09:24 +0000

    Add formatting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ end
 def create  
   @article = Article.new(article_params)  
   if @article.save  
-    flash[:notice] = "Article was successfully created"  
+    flash[:success] = "Article was successfully created"  
     redirect_to article_path(@article)  
   else 
       render 'new'  
@@ -35,14 +35,14 @@ def article_params
 end
 
 def destroy  
-  flash[:notice] = "Article was successfully deleted" 
+  flash[:danger] = "Article was successfully deleted" 
   @article.destroy  
   redirect_to articles_path
 end
 
 def update  
   if @article.update(article_params) 
-    flash[:notice] = "Article was successfully updated"  
+    flash[:success] = "Article was successfully updated"  
     redirect_to article_path(@article)  
   else  
     render 'edit'  
